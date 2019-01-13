721d3bf5117cea376cec06f66cd35b881a86b207
hawkhu <acenudt@foxmail.com>
2019-01-13 21:45:31 +0800

    try fix windows C: driver permission‘s problem

--- res/pasteFromClip.bat
+++ res/pasteFromClip.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d %~dp0
-nircmd.exe clipboard saveimage "c:\temp\temp.png" 
-curl https://sm.ms/api/upload/ -X POST -F "smfile=@c:\temp\temp.png"
+nircmd.exe clipboard saveimage "c:\windows\temp\smms-temp.png" 
+curl https://sm.ms/api/upload/ -X POST -F "smfile=@c:\windows\temp\smms-temp.png"
